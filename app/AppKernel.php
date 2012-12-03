551b15cf2c5cb7d591a41d477f9c2c9c337b54d1
Thomas Loumaigne <loumaignethomas@gmail.com>
2012-12-03 11:40:50 +0100

    creation bundle library

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,8 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Acme\UserBundle\AcmeUserBundle(),
-            new FOS\UserBundle\FOSUserBundle()          
+            new FOS\UserBundle\FOSUserBundle(),
+            new Acme\LibraryBundle\AcmeLibraryBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
